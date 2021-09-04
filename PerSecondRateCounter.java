08802b6bea6d1613705382002fca7cc9259de615
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-09-04 13:47:59 -0500

    Update PerSecondRateCounter.java

--- PerSecondRateCounter.java
+++ PerSecondRateCounter.java
@@ -2,12 +2,11 @@ package com.dz_fs_dev.common;
 
 /**
  * Thread-safe Per Second Rate Counter which employs an Exponential Moving Average with a period defined on construction.
- * @since 15
- * @version 0.1
+ * Exponential moving averages weigh recent data over past data reacting to current event counts over time by decaying past events over time.
+ * 
  * @author DZ-FSDev
- *
+ * @version 0.0.1
  */
-
 public class PerSecondRateCounter {
 	private final int emaPeriod;
 	private final double emaRate, emaRate2;
@@ -16,9 +15,10 @@ public class PerSecondRateCounter {
 
 	/**
 	 * Constructs the Per Second Rate Counter with an Exponential Moving Average.
-	 * @throws IllegalArgumentException Thrown when emaPeriod is less than 2.
-	 * @since 15
+	 * 
 	 * @param emaPeriod 
+	 * @throws IllegalArgumentException Thrown when emaPeriod is less than 2.
+	 * @since 15.0.0.2
 	 */
 	public PerSecondRateCounter(int emaPeriod){
 		if(emaPeriod < 2)throw new IllegalArgumentException("emaPeriod cannot be less than 2.");
@@ -30,7 +30,8 @@ public class PerSecondRateCounter {
 
 	/**
 	 * Increments the counter by 1; thread-safe.
-	 * @since 15
+	 * 
+	 * @since 15.0.0.2
 	 */
 	public synchronized void tick(){
 		count ++;
@@ -39,8 +40,9 @@ public class PerSecondRateCounter {
 	
 	/**
 	 * Increments the counter by a custom amount passed as a parameter; thread-safe.
-	 * @since 15
+	 * 
 	 * @param amount The amount to be incremented in the counter. Supports decimals and negative numbers.
+	 * @since 15.0.0.2
 	 */
 	public synchronized void tick(double amount){
 		count += amount;
@@ -49,7 +51,8 @@ public class PerSecondRateCounter {
 	
 	/**
 	 * Private helper method to calculate the Exponential Moving Average
-	 * @since 15
+	 * 
+	 * @since 15.0.0.2
 	 * @see <a href=https://www.investopedia.com/ask/answers/122314/what-exponential-moving-average-ema-formula-and-how-ema-calculated.asp>
 	 * 		Investopedia - Exponential Moving Average</a>
 	 */
@@ -62,7 +65,8 @@ public class PerSecondRateCounter {
 
 	/**
 	 * Polls the per second exponential moving average rate. Thread-safe.
-	 * @since 15
+	 * 
+	 * @since 15.0.0.2
 	 * @return The per second exponential moving average rate.
 	 */
 	public synchronized double pollPS(){
