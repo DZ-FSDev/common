7b7ba0b51f44d8d1b23c108f1a4ebe03f3911928
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-09-17 08:04:55 -0500

    Update PerSecondRateCounter.java

--- PerSecondRateCounter.java
+++ PerSecondRateCounter.java
@@ -5,9 +5,12 @@ package com.dz_fs_dev.common;
  * Exponential moving averages weigh recent data over past data reacting to current event counts over time by decaying past events over time.
  * 
  * @author DZ-FSDev
+ * @since 16.0.1
  * @version 0.0.1
  */
-public class PerSecondRateCounter {
+public class PerSecondExponentialRateCounter {
+	private static int idGen = 0;
+	private final int id;
 	private final int emaPeriod;
 	private final double emaRate, emaRate2;
 	private double ts;
@@ -16,22 +19,23 @@ public class PerSecondRateCounter {
 	/**
 	 * Constructs the Per Second Rate Counter with an Exponential Moving Average.
 	 * 
-	 * @param emaPeriod 
+	 * @param emaPeriod The exponential moving average period in seconds that will be used to calculate the exponential moving average rates.
 	 * @throws IllegalArgumentException Thrown when emaPeriod is less than 2.
-	 * @since 15.0.0.2
+	 * @since 0.0.1
 	 */
-	public PerSecondRateCounter(int emaPeriod){
-		if(emaPeriod < 2)throw new IllegalArgumentException("emaPeriod cannot be less than 2.");
+	public PerSecondExponentialRateCounter(int emaPeriod){
+		if(emaPeriod < 2)throw new IllegalArgumentException("emaPeriod cannot be less than 2 seconds.");
 		this.emaPeriod = emaPeriod;
 		this.emaRate = 2.0 / (emaPeriod + 1.0);
 		this.emaRate2 = 1.0 - this.emaRate;
 		this.ts = System.currentTimeMillis();
+		this.id = idGen++;
 	}
 
 	/**
 	 * Increments the counter by 1; thread-safe.
 	 * 
-	 * @since 15.0.0.2
+	 * @since 0.0.1
 	 */
 	public synchronized void tick(){
 		count ++;
@@ -42,7 +46,7 @@ public class PerSecondRateCounter {
 	 * Increments the counter by a custom amount passed as a parameter; thread-safe.
 	 * 
 	 * @param amount The amount to be incremented in the counter. Supports decimals and negative numbers.
-	 * @since 15.0.0.2
+	 * @since 0.0.1
 	 */
 	public synchronized void tick(double amount){
 		count += amount;
@@ -52,7 +56,7 @@ public class PerSecondRateCounter {
 	/**
 	 * Private helper method to calculate the Exponential Moving Average
 	 * 
-	 * @since 15.0.0.2
+	 * @since 0.0.1
 	 * @see <a href=https://www.investopedia.com/ask/answers/122314/what-exponential-moving-average-ema-formula-and-how-ema-calculated.asp>
 	 * 		Investopedia - Exponential Moving Average</a>
 	 */
@@ -66,11 +70,31 @@ public class PerSecondRateCounter {
 	/**
 	 * Polls the per second exponential moving average rate. Thread-safe.
 	 * 
-	 * @since 15.0.0.2
+	 * @since 0.0.1
 	 * @return The per second exponential moving average rate.
 	 */
 	public synchronized double pollPS(){
 		tock();
 		return count * 1000 / (System.currentTimeMillis()-ts);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof PerSecondExponentialRateCounter))
+			return false;
+		PerSecondExponentialRateCounter other = (PerSecondExponentialRateCounter) obj;
+		if (id != other.id)
+			return false;
+		return true;
 	}
 }
