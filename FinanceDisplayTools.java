ad598973087c1c3dfe078ab7b3c2f7f7523d8254
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-09-19 09:22:30 -0500

    Update FinanceDisplayTools.java

--- FinanceDisplayTools.java
+++ FinanceDisplayTools.java
@@ -6,13 +6,15 @@ import java.math.BigInteger;
  * Non-contructable class containing different functions used to do pixel calculations and number formatting for financial dashboards and displays.
  * 
  * @author DzFSDev
- * @version 0.1
+ * @since 16
+ * @version 0.0.2
  */
 public final class FinanceDisplayTools {
 	private static String[] fmt_suffixes = {""," K"," M"," B"," T"," A"," C"," D"," E"," F"," G"};
-	
+
 	/**
-	 * Calculates the point on a line segment which a value exists given the terminal ends representing a min and max value. Used for linear scaled data in 1D.
+	 * Calculates the point on a line segment which a value exists given the terminal ends representing a min and max value.
+	 * Used for linear scaled data in 1D.
 	 * 
 	 * @param value	The value that should be placed between the linear line segment. 
 	 * @param min_value The value that corresponds to the start of the line segment.
@@ -20,14 +22,15 @@ public final class FinanceDisplayTools {
 	 * @param startPixel The start of the line segment.
 	 * @param endPixel The end of the line segment.
 	 * @return The point on the line segment which corresponds to the linear position of the value passed.
-	 * @since 15.0.2
+	 * @since 0.0.1
 	 */
 	public static int slider(double value, double min_value, double max_value, double startPixel, double endPixel){
 		return (int)(startPixel + (endPixel-startPixel)*(value-min_value)/(max_value-min_value));
 	}
-	
+
 	/**
-	 * Calculates the point on a line segment which a value exists given the terminal ends representing a min and max value. Used for logarithmic scaled data in 1D.
+	 * Calculates the point on a line segment which a value exists given the terminal ends representing a min and max value.
+	 * Used for logarithmic scaled data in 1D.
 	 * 
 	 * @param value	The value that should be placed between the linear line segment. 
 	 * @param min_value The value that corresponds to the start of the line segment.
@@ -35,7 +38,7 @@ public final class FinanceDisplayTools {
 	 * @param startPixel The start of the line segment.
 	 * @param endPixel The end of the line segment.
 	 * @return The point on the line segment which corresponds to the logarithmic position of the value passed.
-	 * @since 15.0.2
+	 * @since 0.0.1
 	 */
 	public static int logSlider(double value, double min_value, double max_value, double startPixel, double endPixel){
 		value=Math.log(value);
@@ -43,31 +46,50 @@ public final class FinanceDisplayTools {
 		max_value=Math.log(max_value);
 		return (int)(startPixel + (endPixel-startPixel)*(value-min_value)/(max_value-min_value));
 	}
-	
+
 	/**
 	 * Formats a double precision value into a string up to a specified precision. Rounds down.
 	 * 
 	 * @param d The double precision value to be formatted.
 	 * @param prec The number of decimal precision to return.
-	 * @return The formatted string representation.
-	 * @since 15.0.2
+	 * @return The formatted {@link String} representation.
+	 * @since 0.0.1
 	 */
 	public static String formatDbl(double d, int prec) {
 		return String.format("%." + prec + "f",d);
 	}
 
 	/**
-	 * Formats a {@link BigInteger} into more relatable string representation up to a specific precision.
+	 * Formats a {@link BigInteger} into more relatable {@link String} representation up to a specific precision.
 	 * 
 	 * @param bi The {@link BigInteger} to be formatted.
 	 * @param prec The precision of the formatted number.
-	 * @return The string representation of the formatted number.
-	 * @since 15.0.2
+	 * @return The {@link String} representation of the formatted number.
+	 * @since 0.0.1
 	 */
-	public static String fmt(BigInteger bi, int prec){
+	public static String formatBigInt(BigInteger bi, int prec){
 		String ret = bi.toString();
 		if(ret.length() < prec)return ret;
 		int suf = (ret.length()-1) / 3;
 		return ret.substring(0,ret.length()-suf*3) + "." + ret.substring(ret.length()-suf*3,5) + fmt_suffixes[suf];
+	}
+
+	/**
+	 * Performs the calculation of numerator divided by denominator and converts the result into a percentage through multiplication of 100.
+	 * The result is trimmed to the specified decimal precision by calling {@link #formatDbl(double, int) formatDbl}.
+	 * NaN is return if a denominator of zero is passed. No Exception will be thrown.
+	 * IMPORTANT: This method uses primitve double calculations. For mission critical applications, use {@link formatBigRatio}
+	 * 
+	 * @param numerator The numerator of the ratio.
+	 * @param denominator The denominator of the ratio.
+	 * @param prec The number of decimal precision to return.
+	 * @return The formatted {@link String} representation.
+	 * @throws IllegalArgumentException Thrown when precision is less than 1
+	 * @since 0.0.2
+	 */
+	public static String formatRatio(double numerator, double denominator, int prec) throws IllegalArgumentException{
+		if(denominator == 0)return ""+Double.NaN;
+		if(prec < 1)throw new IllegalArgumentException("fmtRatio cannot process a precision(" + prec + ") less than 1!");
+		return formatDbl(numerator/denominator*100.0, prec);
 	}
 }
