a56ffa681cc852db902b5f2beebf02a1061c16dc
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-11-27 12:25:50 -0600

    Update Graphics2DTools.java

--- Graphics2DTools.java
+++ Graphics2DTools.java
@@ -3,15 +3,21 @@ package com.dz_fs_dev.common;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 /**
  * Non-contructable class containing different image manipulative methods for financial dashboards and displays.
  * 
  * @author DZ-FSDev
- * @since 16.0.1
- * @version 0.0.1
+ * @since 17.0.1
+ * @version 0.0.2
  */
 public final class Graphics2DTools {
+	private Graphics2DTools() {}
+	
 	/**
 	 * Scales an image retaining lines and parallelism at the cost of distances and angles between objects.
 	 * 
@@ -19,6 +25,7 @@ public final class Graphics2DTools {
 	 * @param scaledWidth The width of scaled image.
 	 * @param scaledHeight The height of scaled image.
 	 * @return The affine scaled image.
+	 * @since 0.0.1
 	 */
 	public static BufferedImage scaleImageEuclidianAffine(BufferedImage sourceImg, int scaledWidth, int scaledHeight) {
 		BufferedImage scaledImg = null;
@@ -38,6 +45,7 @@ public final class Graphics2DTools {
 	 * @param scaledWidth The width of scaled image.
 	 * @param scaledHeight The height of scaled image.
 	 * @return The scaled image.
+	 * @since 0.0.1
 	 */
 	public static BufferedImage scaleImageFast(BufferedImage sourceImg, int scaledWidth, int scaledHeight) {
 		BufferedImage scaledImg = new BufferedImage(scaledWidth, scaledHeight, sourceImg.getType());
@@ -45,5 +53,31 @@ public final class Graphics2DTools {
 		g2d.drawImage(sourceImg, 0, 0, scaledWidth, scaledHeight, null);
 		g2d.dispose();
 		return scaledImg;
+	}
+	
+	/**
+	 * Saves a BufferedImage to a specified file as a portable network graphic.
+	 * 
+	 * @param bufferedImage The image to be saved.
+	 * @param fileName The name of the file to be saved.
+	 * @throws IOException Thrown if the file cannot be written.
+	 * @since 0.0.2
+	 */
+	public static void saveAsPNG(BufferedImage bufferedImage, String fileName) throws IOException {
+		File outputfile = new File(fileName + ".png");
+		ImageIO.write(bufferedImage, "png", outputfile);
+	}
+	
+	/**
+	 * Saves a BufferedImage to a specified file as a Joint Photographic Experts Group digital image.
+	 * 
+	 * @param bufferedImage The image to be saved.
+	 * @param fileName The name of the file to be saved.
+	 * @throws IOException Thrown if the file cannot be written.
+	 * @since 0.0.2
+	 */
+	public static void saveAsJPG(BufferedImage bufferedImage, String fileName) throws IOException {
+		File outputfile = new File(fileName + ".jpg");
+		ImageIO.write(bufferedImage, "jpg", outputfile);
 	}
 }
